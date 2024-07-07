374eabaf519170495d32f17d71de0e8704e350c7
mehmtmre <memrecm@gmail.com>
2024-07-07 17:31:25 +0300

    Initial Commit

---
+++ RestartApp/RestartAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartAppApp.swift
+//  RestartApp
+//
+//  Created by Mehmet Emre Çimenlik on 7.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct RestartAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
